7b502cd74a0eedc9667c032b10f964b34ed3aef1
g5g1ta <gogo.gogo.1@abv.bg>
2025-02-27 22:51:23 +0200

    Fixed The Design for the PhotoList and fixed a typo in requirements.txt

--- Frontend/src/pages/PhotoCard.jsx
+++ Frontend/src/pages/PhotoCard.jsx
@@ -5,11 +5,7 @@ function PhotoCard(props){
 
     return (
         <div className="card">
-            <img className="img" src={`http://localhost:8000/media/user_images/${props.image}`} alt="Avatar"/>
-            <div className="container">
-                <h4><b>{props.name}</b></h4>
-                <button>Edit</button>
-            </div>
+            <img className="img1" src={`http://localhost:8000/media/user_images/${props.image}`} alt="Avatar"/>
         </div>
     )
 }
